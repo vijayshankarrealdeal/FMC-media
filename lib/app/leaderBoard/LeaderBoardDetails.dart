1736e01f6b06de9b43fa403401097faf892af64d
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-04-17 00:55:34 +0530

    BanGaya Mast

--- lib/app/leaderBoard/LeaderBoardDetails.dart
+++ lib/app/leaderBoard/LeaderBoardDetails.dart
@@ -1,12 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fmc/model/leaderBoardModel.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class LeaderBoardExtensionWith3Images extends StatelessWidget {
   final LeaderBoardModel data;
 
-  const LeaderBoardExtensionWith3Images({Key key, this.data}) : super(key: key);
+  LeaderBoardExtensionWith3Images({Key key, this.data}) : super(key: key);
+  List<String> m = [];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    m.add(data.fisrt);
+    data.second.isNotEmpty ? m.add(data.second) : print("NotHere");
+    data.third.isNotEmpty ? m.add(data.third) : print("NotHere");
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: CupertinoColors.black,
+          body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Carousel(
+              images: m.map((e) => CachedNetworkImage(imageUrl: e)).toList(),
+              autoplay: false,
+            ),
+          )),
+    );
   }
 }
